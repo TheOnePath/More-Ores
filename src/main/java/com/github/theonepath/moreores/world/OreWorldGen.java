d485fa886e966b199766f522da05b85924523f39
TheOnePath <35313544+TheOnePath@users.noreply.github.com>
2020-05-29 18:05:14 +0100

    Changed for 1.15

--- src/main/java/com/github/theonepath/moreores/world/OreWorldGen.java
+++ src/main/java/com/github/theonepath/moreores/world/OreWorldGen.java
@@ -2,38 +2,35 @@ package com.github.theonepath.moreores.world;
 
 import com.github.theonepath.moreores.lists.BlockList;
 
+import com.github.theonepath.moreores.setup.Config;
+import net.minecraft.block.Block;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.gen.GenerationStage.Decoration;
+import net.minecraft.world.gen.OverworldGenSettings;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
 import net.minecraft.world.gen.feature.OreFeatureConfig.FillerBlockType;
 import net.minecraft.world.gen.placement.CountRangeConfig;
 import net.minecraft.world.gen.placement.Placement;
+import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.registries.ForgeRegistries;
 
-public class OreWorldGen{
-	private static CountRangeConfig copper_ore_placement = new CountRangeConfig(8, 20, 0, 80);
-	private static CountRangeConfig alum_ore_placement = new CountRangeConfig(3, 5, 0, 40);
-	private static CountRangeConfig lead_ore_placement = new CountRangeConfig(4, 5, 0, 45);
-	private static CountRangeConfig nickel_ore_placement = new CountRangeConfig(3, 2, 0, 30);
-	private static CountRangeConfig silver_ore_placement = new CountRangeConfig(4, 15, 0, 50);
-	private static CountRangeConfig tin_ore_placement = new CountRangeConfig(6, 20, 0, 70);
-	private static CountRangeConfig zinc_ore_placement = new CountRangeConfig(4, 5, 0, 30);
+import java.util.HashMap;
+import java.util.Map;
 
+public class OreWorldGen{
 	public static void setupOreWorldGen() {
 		for (Biome biome : ForgeRegistries.BIOMES.getValues()) {
-			if (  biome.getCategory() == Biome.Category.THEEND || biome.getCategory() == Biome.Category.NETHER)
-            {
-                continue;
-            }
-			
-			biome.addFeature(Decoration.UNDERGROUND_ORES, Biome.createDecoratedFeature(Feature.ORE, new OreFeatureConfig(FillerBlockType.NATURAL_STONE, BlockList.COPPER_ORE.getDefaultState(), 10), Placement.COUNT_RANGE, copper_ore_placement));
-			biome.addFeature(Decoration.UNDERGROUND_ORES, Biome.createDecoratedFeature(Feature.ORE, new OreFeatureConfig(FillerBlockType.NATURAL_STONE, BlockList.ALUM_ORE.getDefaultState(), 10), Placement.COUNT_RANGE, alum_ore_placement));
-			biome.addFeature(Decoration.UNDERGROUND_ORES, Biome.createDecoratedFeature(Feature.ORE, new OreFeatureConfig(FillerBlockType.NATURAL_STONE, BlockList.LEAD_ORE.getDefaultState(), 10), Placement.COUNT_RANGE, lead_ore_placement));
-			biome.addFeature(Decoration.UNDERGROUND_ORES, Biome.createDecoratedFeature(Feature.ORE, new OreFeatureConfig(FillerBlockType.NATURAL_STONE, BlockList.NICKEL_ORE.getDefaultState(), 10), Placement.COUNT_RANGE, nickel_ore_placement));
-			biome.addFeature(Decoration.UNDERGROUND_ORES, Biome.createDecoratedFeature(Feature.ORE, new OreFeatureConfig(FillerBlockType.NATURAL_STONE, BlockList.SILVER_ORE.getDefaultState(), 10), Placement.COUNT_RANGE, silver_ore_placement));
-			biome.addFeature(Decoration.UNDERGROUND_ORES, Biome.createDecoratedFeature(Feature.ORE, new OreFeatureConfig(FillerBlockType.NATURAL_STONE, BlockList.TIN_ORE.getDefaultState(), 10), Placement.COUNT_RANGE, tin_ore_placement));
-			biome.addFeature(Decoration.UNDERGROUND_ORES, Biome.createDecoratedFeature(Feature.ORE, new OreFeatureConfig(FillerBlockType.NATURAL_STONE, BlockList.ZINC_ORE.getDefaultState(), 10), Placement.COUNT_RANGE, zinc_ore_placement));
+			if ( biome.getCategory() != Biome.Category.THEEND || biome.getCategory() != Biome.Category.NETHER) {
+				for (Map.Entry<Block, Map<CountRangeConfig, Integer>> entry : Config.ORES_DATA.entrySet()) {
+					for (Map.Entry<CountRangeConfig, Integer> subentry : entry.getValue().entrySet()) {
+						biome.addFeature(Decoration.UNDERGROUND_ORES, Feature.ORE
+								.withConfiguration(new OreFeatureConfig(FillerBlockType.NATURAL_STONE, entry.getKey().getDefaultState(), subentry.getValue()))
+								.withPlacement(Placement.COUNT_RANGE.configure(subentry.getKey())));
+					}
+				}
+			}
 		}
 	}
 }
