f2852d23f10b110627a9711fec4a329385af6e9d
TheOnePath <35313544+TheOnePath@users.noreply.github.com>
2020-06-03 20:22:56 +0100

    Update OreWorldGen.java
    Ameneded approriate changes for generating ores

--- src/main/java/com/github/theonepath/moreores/world/OreWorldGen.java
+++ src/main/java/com/github/theonepath/moreores/world/OreWorldGen.java
@@ -1,34 +1,30 @@
 package com.github.theonepath.moreores.world;
 
-import com.github.theonepath.moreores.lists.BlockList;
-
-import com.github.theonepath.moreores.setup.Config;
+import com.github.theonepath.moreores.setup.WorldOreConfig;
 import net.minecraft.block.Block;
 import net.minecraft.world.biome.Biome;
-import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.gen.GenerationStage.Decoration;
-import net.minecraft.world.gen.OverworldGenSettings;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
 import net.minecraft.world.gen.feature.OreFeatureConfig.FillerBlockType;
 import net.minecraft.world.gen.placement.CountRangeConfig;
 import net.minecraft.world.gen.placement.Placement;
-import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.registries.ForgeRegistries;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class OreWorldGen{
 	public static void setupOreWorldGen() {
 		for (Biome biome : ForgeRegistries.BIOMES.getValues()) {
 			if ( biome.getCategory() != Biome.Category.THEEND || biome.getCategory() != Biome.Category.NETHER) {
-				for (Map.Entry<Block, Map<CountRangeConfig, Integer>> entry : Config.ORES_DATA.entrySet()) {
-					for (Map.Entry<CountRangeConfig, Integer> subentry : entry.getValue().entrySet()) {
-						biome.addFeature(Decoration.UNDERGROUND_ORES, Feature.ORE
-								.withConfiguration(new OreFeatureConfig(FillerBlockType.NATURAL_STONE, entry.getKey().getDefaultState(), subentry.getValue()))
-								.withPlacement(Placement.COUNT_RANGE.configure(subentry.getKey())));
-					}
+				for (Map.Entry<Block, Map<CountRangeConfig, Integer>> entry : WorldOreConfig.ORES_DATA.entrySet()) {
+					try {
+						for (Map.Entry<CountRangeConfig, Integer> subentry : entry.getValue().entrySet()) {
+							biome.addFeature(Decoration.UNDERGROUND_ORES, Feature.ORE
+									.withConfiguration(new OreFeatureConfig(FillerBlockType.NATURAL_STONE, entry.getKey().getDefaultState(), subentry.getValue()))
+									.withPlacement(Placement.COUNT_RANGE.configure(subentry.getKey())));
+						}
+					} catch (Exception e) { throw e; }
 				}
 			}
 		}
