3a040866cbec15c07704828563b33f67fe4e8e3e
TheOnePath <35313544+TheOnePath@users.noreply.github.com>
2020-06-03 20:32:10 +0100

    Created Generator
    Created Generator to burn coke with a lose factor. Used to generate Forge Energy

---
+++ src/main/java/com/github/theonepath/moreores/blocks/Generator.java
@@ -0,0 +1,109 @@
+package com.github.theonepath.moreores.blocks;
+
+import com.github.theonepath.moreores.blocks.tileentity.GeneratorTileEntity;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.inventory.container.INamedContainerProvider;
+import net.minecraft.item.ItemStack;
+import net.minecraft.particles.ParticleTypes;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorldReader;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.fml.network.NetworkHooks;
+
+import javax.annotation.Nullable;
+import java.util.Random;
+
+import static net.minecraft.state.properties.BlockStateProperties.FACING;
+
+public class Generator extends Block {
+
+    public Generator() {
+        super(Properties.create(Material.ROCK)
+                .hardnessAndResistance(2.0f)
+                .lightValue(14)
+        );
+        setRegistryName("generator");
+    }
+
+    @Override
+    public boolean hasTileEntity(BlockState state) {
+        return true;
+    }
+
+    @Override
+    public int getLightValue(BlockState state, IBlockReader world, BlockPos pos) {
+        return state.get(BlockStateProperties.POWERED) ? super.getLightValue(state, world, pos) : 0;
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public void animateTick(BlockState stateIn, World worldIn, BlockPos pos, Random rand) {
+        if (stateIn.get(BlockStateProperties.POWERED)) {
+            double d0 = (double)pos.getX() + 0.5D;
+            double d1 = pos.getY();
+            double d2 = (double)pos.getZ() + 0.5D;
+
+            if (rand.nextDouble() < 0.1D) {
+                worldIn.playSound(d0, d1, d2, SoundEvents.BLOCK_FURNACE_FIRE_CRACKLE, SoundCategory.BLOCKS, 1.0F, 1.0F, false);
+            }
+
+            Direction direction = stateIn.get(FACING);
+            Direction.Axis direction$axis = direction.getAxis();
+
+            double d4 = rand.nextDouble() * 0.6D - 0.3D;
+            double d5 = direction$axis == Direction.Axis.X ? (double)direction.getXOffset() * 0.52D : d4;
+            double d6 = rand.nextDouble() * 6.0D / 16.0D;
+            double d7 = direction$axis == Direction.Axis.Z ? (double)direction.getZOffset() * 0.52D : d4;
+
+            worldIn.addParticle(ParticleTypes.SMOKE, d0 + d5, d1 + d6, d2 + d7, 0.0D, 0.0D, 0.0D);
+            worldIn.addParticle(ParticleTypes.FLAME, d0 + d5, d1 + d6, d2 + d7, 0.0D, 0.0D, 0.0D);
+        }
+    }
+
+    @Nullable
+    @Override
+    public TileEntity createTileEntity(BlockState state, IBlockReader world) {
+        return new GeneratorTileEntity();
+    }
+
+    @Override
+    public void onBlockPlacedBy(World worldIn, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack stack) {
+        if (placer != null) {
+            worldIn.setBlockState(pos, state.with(FACING, getFacingFromEntity(pos, placer)), 2);
+        }
+    }
+
+    @Override
+    public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
+        if (!worldIn.isRemote) {
+            TileEntity tileEntity = worldIn.getTileEntity(pos);
+            if (tileEntity instanceof INamedContainerProvider) {
+                NetworkHooks.openGui((ServerPlayerEntity) player, (INamedContainerProvider) tileEntity, tileEntity.getPos());
+            }
+        }
+        return super.onBlockActivated(state, worldIn, pos, player, handIn, hit);
+    }
+
+    public static Direction getFacingFromEntity(BlockPos clickedBlock, LivingEntity entity){
+        return Direction.getFacingFromVector((float) (entity.getPosX() - clickedBlock.getX()), (float) (entity.getPosY() - clickedBlock.getY()), (float) (entity.getPosZ() - clickedBlock.getZ()));
+    }
+
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(FACING, BlockStateProperties.POWERED);
+    }
+}
---
+++ src/main/java/com/github/theonepath/moreores/blocks/gui/GeneratorScreen.java
@@ -0,0 +1,44 @@
+package com.github.theonepath.moreores.blocks.gui;
+
+import com.github.theonepath.moreores.blocks.container.GeneratorContainer;
+import com.mojang.blaze3d.platform.GlStateManager;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screen.inventory.ContainerScreen;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.ITextComponent;
+
+import static com.github.theonepath.moreores.MoreOres.MODID;
+
+public class GeneratorScreen extends ContainerScreen<GeneratorContainer> {
+
+    private ResourceLocation GUI = new ResourceLocation(MODID, "textures/gui/generator.png");
+
+    public GeneratorScreen(GeneratorContainer container, PlayerInventory inventory, ITextComponent component) {
+        super(container, inventory, component);
+    }
+
+    @Override
+    public void render(int mouseX, int mouseY, float partialTick) {
+        this.renderBackground();
+        super.render(mouseX, mouseY, partialTick);
+        this.renderHoveredToolTip(mouseX, mouseY);
+    }
+
+    @Override
+    protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
+        this.font.drawString(this.title.getFormattedText(), 8.0f, 6.0f, 4210752);
+        this.font.drawString(this.playerInventory.getDisplayName().getFormattedText(), 8.0f, (float)(this.ySize - 108 + 2), 4210752);
+        drawString(Minecraft.getInstance().fontRenderer, "Energy: " + container.getEnergy(), 64 + 28, 29, 0xffffff);
+    }
+
+    @Override
+    protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
+        GlStateManager.blendColor(1.0f, 1.0f, 1.0f, 1.0f);
+        this.minecraft.getTextureManager().bindTexture(GUI);
+
+        int relX = (this.width - this.xSize) / 2;
+        int relY = (this.height - this.ySize) / 2;
+        this.blit(relX, relY, 0, 0, this.xSize, this.ySize);
+    }
+}
