1a19f3a8ab464f3e452f12b4dfdf88133f24c00d
TheOnePath <35313544+TheOnePath@users.noreply.github.com>
2020-06-03 20:32:51 +0100

    Create ToolList.java

---
+++ src/main/java/com/github/theonepath/moreores/lists/ToolList.java
@@ -0,0 +1,31 @@
+package com.github.theonepath.moreores.lists;
+
+import com.github.theonepath.moreores.MoreOres;
+import com.github.theonepath.moreores.items.tools.*;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.registries.ObjectHolder;
+
+public class ToolList {
+    private static final String id = "moreores";
+    private static final ItemGroup group = MoreOres.setup.itemGroup;
+
+    // Tools
+    @ObjectHolder(id + ":rock_grinder_stone")
+    public static Item ROCK_GRINDER_STONE = new RockGrinderStone(ToolMaterialList.rock_grinder_stone_material, 4, -2.8F, (new Item.Properties()).group(group));
+
+    @ObjectHolder(id + ":rock_grinder_iron")
+    public static Item ROCK_GRINDER_IRON = new RockGrinderIron(ToolMaterialList.rock_grinder_iron_material, 5, -2.8F, (new Item.Properties()).group(group));
+
+    @ObjectHolder(id + ":rock_grinder_gold")
+    public static Item ROCK_GRINDER_GOLD = new RockGrinderGold(ToolMaterialList.rock_grinder_gold_material, 3, -2.8F, (new Item.Properties()).group(group));
+
+    @ObjectHolder(id + ":rock_grinder_diamond")
+    public static Item ROCK_GRINDER_DIAMOND = new RockGrinderDiamond(ToolMaterialList.rock_grinder_diamond_material, 6, -2.8F, (new Item.Properties()).group(group));
+
+    @ObjectHolder(id + ":rock_grinder_emerald")
+    public static Item ROCK_GRINDER_EMERALD = new RockGrinderEmerald(ToolMaterialList.rock_grinder_emerald_material, 6, -2.8F, (new Item.Properties()).group(group));
+
+    @ObjectHolder(id + ":rock_grinder_unbreakable")
+    public static Item ROCK_GRINDER_UNBREAKABLE = new RockGrinderUnbreakable(ToolMaterialList.rock_grinder_unbreakable_material, 7, -2.6F, (new Item.Properties()).group(group));
+}
