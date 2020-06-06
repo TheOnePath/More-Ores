335b784d7aa6255e2cbe4ff14d75681ba3f90981
TheOnePath <35313544+TheOnePath@users.noreply.github.com>
2020-06-06 01:07:45 +0100

    Update PowerBankTileEntity.java

--- src/main/java/com/github/theonepath/moreores/blocks/tileentity/PowerBankTileEntity.java
+++ src/main/java/com/github/theonepath/moreores/blocks/tileentity/PowerBankTileEntity.java
@@ -3,16 +3,19 @@ package com.github.theonepath.moreores.blocks.tileentity;
 import com.github.theonepath.moreores.Config;
 import com.github.theonepath.moreores.blocks.container.PowerBankContainer;
 import com.github.theonepath.moreores.lists.BlockList;
+import com.github.theonepath.moreores.lists.ItemList;
 import com.github.theonepath.moreores.tools.CustomEnergyStorage;
 import javafx.geometry.Side;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.container.Container;
 import net.minecraft.inventory.container.INamedContainerProvider;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.common.capabilities.Capability;
@@ -20,6 +23,9 @@ import net.minecraftforge.common.util.INBTSerializable;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.energy.CapabilityEnergy;
 import net.minecraftforge.energy.IEnergyStorage;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -27,15 +33,14 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class PowerBankTileEntity extends TileEntity implements ITickableTileEntity, INamedContainerProvider, IEnergyStorage {
 
+    private LazyOptional<IItemHandler> handler = LazyOptional.of(this::createHandler);
     private LazyOptional<IEnergyStorage> energy = LazyOptional.of(this::createEnergy);
+
     public static final int SIZE = 1;
 
     public PowerBankTileEntity() {
         super(BlockList.POWERBANK_TILE);
-    }
-
-    private IEnergyStorage createEnergy(){
-        return new CustomEnergyStorage(Config.POWERBANK_MAXPOWER.get(), 0);
+        System.out.println("Power Bank Tile Entity");
     }
 
     @Override
@@ -92,9 +97,31 @@ public class PowerBankTileEntity extends TileEntity implements ITickableTileEnti
         return super.write(tag);
     }
 
+    private IItemHandler createHandler() {
+        return new ItemStackHandler(1) {
+
+            @Override
+            protected void onContentsChanged(int slot) {
+                markDirty();
+            }
+
+            @Override
+            public boolean isItemValid(int slot, @Nonnull ItemStack stack) {
+                return false;
+            }
+        };
+    }
+
+    private IEnergyStorage createEnergy(){
+        return new CustomEnergyStorage(Config.POWERBANK_MAXPOWER.get(), 0);
+    }
+
     @Nonnull
     @Override
     public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
+        if (cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
+            return handler.cast();
+        }
         if (cap == CapabilityEnergy.ENERGY) {
             return energy.cast();
         }
