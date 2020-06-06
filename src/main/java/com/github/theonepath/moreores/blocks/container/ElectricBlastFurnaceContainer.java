e4277777e50b5b6ddb9178ba70c0aa1662f34561
TheOnePath <35313544+TheOnePath@users.noreply.github.com>
2020-06-06 01:04:47 +0100

    Created ElectricBlastFurnace
    Created a block called ElectricBlastFurnace to use FE to smelt items (WIP).

---
+++ src/main/java/com/github/theonepath/moreores/blocks/container/ElectricBlastFurnaceContainer.java
@@ -0,0 +1,122 @@
+package com.github.theonepath.moreores.blocks.container;
+
+import com.github.theonepath.moreores.blocks.tileentity.ElectricBlastFurnaceTileEntity;
+import com.github.theonepath.moreores.lists.BlockList;
+import com.github.theonepath.moreores.tools.CustomEnergyStorage;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.inventory.container.Slot;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IWorldPosCallable;
+import net.minecraft.util.IntReferenceHolder;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.energy.CapabilityEnergy;
+import net.minecraftforge.energy.IEnergyStorage;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.SlotItemHandler;
+import net.minecraftforge.items.wrapper.InvWrapper;
+
+import static com.github.theonepath.moreores.lists.BlockList.ELECTRIC_BLAST_FURNACE_CONTAINER;
+
+public class ElectricBlastFurnaceContainer extends Container {
+
+    private TileEntity tileEntity;
+    private PlayerEntity playerEntity;
+    private IItemHandler playerInventory;
+
+    public ElectricBlastFurnaceContainer(int windowId, World world, BlockPos pos, PlayerInventory inventory, PlayerEntity player) {
+        super(ELECTRIC_BLAST_FURNACE_CONTAINER, windowId);
+        tileEntity = world.getTileEntity(pos);
+
+        this.playerEntity = player;
+        this.playerInventory = new InvWrapper(inventory);
+
+        tileEntity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY).ifPresent(h -> {
+            addSlot(new SlotItemHandler(h, 0, 46, 27));
+            /*addSlot(new SlotItemHandler(h, 1, 53, 7));
+            addSlot(new SlotItemHandler(h, 2, 53, 27));
+            addSlot(new SlotItemHandler(h, 3, 53, 47));*/
+        });
+        layoutPlayerInventorySlots(10, 70);
+
+        trackInt(new IntReferenceHolder() {
+            @Override
+            public int get() {
+                return getEnergy();
+            }
+
+            @Override
+            public void set(int value) {
+                tileEntity.getCapability(CapabilityEnergy.ENERGY).ifPresent(h -> ((CustomEnergyStorage) h).setEnergy(value));
+            }
+        });
+    }
+
+    public int getEnergy() {
+        return tileEntity.getCapability(CapabilityEnergy.ENERGY).map(IEnergyStorage::getEnergyStored).orElse(0);
+    }
+
+    @Override
+    public boolean canInteractWith(PlayerEntity playerIn) {
+        return isWithinUsableDistance(IWorldPosCallable.of(tileEntity.getWorld(), tileEntity.getPos()), playerEntity, BlockList.ELECTRIC_BLAST_FURNACE);
+    }
+
+    // Implement this method to move items in and out of the slot for shift-clicking
+    @Override
+    public ItemStack transferStackInSlot(PlayerEntity playerIn, int index) {
+        ItemStack itemstack = ItemStack.EMPTY;
+        Slot slot = this.inventorySlots.get(index);
+
+        if (slot != null && slot.getHasStack()) {
+            ItemStack itemstack1 = slot.getStack();
+            itemstack = itemstack1.copy();
+
+            if (index < ElectricBlastFurnaceTileEntity.SIZE) {
+                if (!this.mergeItemStack(itemstack1, ElectricBlastFurnaceTileEntity.SIZE, this.inventorySlots.size(), true)) {
+                    return ItemStack.EMPTY;
+                }
+            } else if (!this.mergeItemStack(itemstack1, 0, ElectricBlastFurnaceTileEntity.SIZE, false)) {
+                return ItemStack.EMPTY;
+            }
+
+            if (itemstack1.isEmpty()) {
+                slot.putStack(ItemStack.EMPTY);
+            } else {
+                slot.onSlotChanged();
+            }
+        }
+
+        return itemstack;
+    }
+
+    private int addSlotRange(IItemHandler handler, int index, int x, int y, int amount, int dx) {
+        for (int i = 0 ; i < amount ; i++) {
+            addSlot(new SlotItemHandler(handler, index, x, y));
+            x += dx;
+            index++;
+        }
+
+        return index;
+    }
+
+    private int addSlotBox(IItemHandler handler, int index, int x, int y, int horAmount, int dx, int verAmount, int dy){
+        for (int j = 0 ; j < verAmount ; j++) {
+            index = addSlotRange(handler, index, x, y, horAmount, dx);
+            y += dy;
+        }
+
+        return index;
+    }
+
+    private void layoutPlayerInventorySlots(int leftCol, int topRow) {
+        addSlotBox(playerInventory, 9, leftCol, topRow, 9, 18, 3, 18);
+
+        // Hotbar
+        topRow += 58;
+        addSlotRange(playerInventory, 0, leftCol, topRow, 9, 18);
+    }
+}
---
+++ src/main/java/com/github/theonepath/moreores/blocks/tileentity/ElectricBlastFurnaceTileEntity.java
@@ -0,0 +1,166 @@
+package com.github.theonepath.moreores.blocks.tileentity;
+
+import com.github.theonepath.moreores.Config;
+import com.github.theonepath.moreores.blocks.container.ElectricBlastFurnaceContainer;
+import com.github.theonepath.moreores.lists.BlockList;
+import com.github.theonepath.moreores.lists.ItemList;
+import com.github.theonepath.moreores.tools.CustomEnergyStorage;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.inventory.container.IContainerProvider;
+import net.minecraft.inventory.container.INamedContainerProvider;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.tileentity.AbstractFurnaceTileEntity;
+import net.minecraft.tileentity.ITickableTileEntity;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Direction;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.INBTSerializable;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.energy.CapabilityEnergy;
+import net.minecraftforge.energy.IEnergyStorage;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+
+public class ElectricBlastFurnaceTileEntity extends TileEntity implements ITickableTileEntity, INamedContainerProvider, IEnergyStorage {
+
+    private LazyOptional<IItemHandler> handler = LazyOptional.of(this::createHandler);
+    private LazyOptional<IEnergyStorage> energy = LazyOptional.of(this::createEnergy);
+
+    public static final int SIZE = 4;
+    private int tickCounter;
+
+    public ElectricBlastFurnaceTileEntity() {
+        super(BlockList.ELECTRIC_BLAST_FURNACE_TILE);
+    }
+
+    @Override
+    public void tick() {
+        if (world.isRemote) {
+            return;
+        }
+
+        handler.ifPresent(h -> {
+            BlockState blockState = world.getBlockState(pos);
+
+
+            // set BlockState to powered if it isn't already and tickCounter is greater than 0 and max energy isn't reached
+            if (blockState.get(BlockStateProperties.POWERED) != tickCounter > 0) {
+                world.setBlockState(pos, blockState.with(BlockStateProperties.POWERED, tickCounter > 0), 3);
+            }
+            energy.ifPresent(m -> { });
+        });
+    }
+
+
+    @Override
+    public void read(CompoundNBT tag) {
+        CompoundNBT invTag = tag.getCompound("inv");
+        handler.ifPresent(h -> ((INBTSerializable<CompoundNBT>) h).deserializeNBT(invTag));
+
+        CompoundNBT energyTag = tag.getCompound("energy");
+        energy.ifPresent(h -> ((INBTSerializable<CompoundNBT>) h).deserializeNBT(energyTag));
+        super.read(tag);
+    }
+
+
+    @Override
+    public CompoundNBT write(CompoundNBT tag) {
+        handler.ifPresent(h -> {
+            CompoundNBT compound = ((INBTSerializable<CompoundNBT>) h).serializeNBT();
+            tag.put("inv", compound);
+        });
+        energy.ifPresent(h -> {
+            CompoundNBT compound = ((INBTSerializable<CompoundNBT>) h).serializeNBT();
+            tag.put("energy", compound);
+        });
+
+        return super.write(tag);
+    }
+
+
+    private IItemHandler createHandler() {
+        return new ItemStackHandler(1) {
+
+            @Override
+            protected void onContentsChanged(int slot) {
+                markDirty();
+            }
+
+            @Override
+            public boolean isItemValid(int slot, @Nonnull ItemStack stack) {
+                return stack.getItem() == ItemList.COPPER_CHUNK;
+            }
+        };
+    }
+
+    private IEnergyStorage createEnergy() {
+        return new CustomEnergyStorage(Config.ELECTRIC_BLAST_FURNACE_MAXPOWER.get(), 0);
+    }
+
+
+    @Nonnull
+    @Override
+    public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
+        if (cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
+            return handler.cast();
+        }
+        if (cap == CapabilityEnergy.ENERGY) {
+            return energy.cast();
+        }
+        return super.getCapability(cap, side);
+    }
+
+    @Override
+    public ITextComponent getDisplayName() {
+        return new TranslationTextComponent("container.moreores.electric_blast_furnace");
+    }
+
+    @Nullable
+    @Override
+    public Container createMenu(int i, PlayerInventory inventory, PlayerEntity entity) {
+        return new ElectricBlastFurnaceContainer(i, world, pos, inventory, entity);
+    }
+
+    @Override
+    public int receiveEnergy(int maxReceive, boolean simulate) {
+        return maxReceive;
+    }
+
+    @Override
+    public int extractEnergy(int maxExtract, boolean simulate) {
+        return 0;
+    }
+
+    @Override
+    public int getEnergyStored() {
+        return getEnergyStored();
+    }
+
+    @Override
+    public int getMaxEnergyStored() {
+        return getMaxEnergyStored();
+    }
+
+    @Override
+    public boolean canExtract() {
+        return false;
+    }
+
+    @Override
+    public boolean canReceive() {
+        return true;
+    }
+}
+
